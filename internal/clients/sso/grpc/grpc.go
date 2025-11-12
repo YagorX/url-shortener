1f1c9ddab8a0e22d61e5a8c8e395746f4e9220ca
YagorX <popa256666@icloud.com>
2025-11-12 17:22:30 +0300

    first version, bug with connect to sso

---
+++ internal/clients/sso/grpc/grpc.go
@@ -0,0 +1,44 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	ssov1 "github.com/YagorX/protos/gen/go/sso"
+	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceprots/logging"
+	grpcretry "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceprots/retry"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	api ssov1.AuthClient
+	log *slog.Logger
+}
+
+func New(ctx context.Context,
+	log *slog.Logger,
+	addr string,
+	timeout time.Duration,
+	retriesCount int,
+) (*Client, error) {
+	const op = "grpc.New"
+
+	retryOpts := []grpcretry.CallOption{
+		grpcretry.WithCodes(codes.NotFound, codes.Aborted, codes.DeadlineExceeded),
+		grpcretry.WithMax(uint(retriesCount)),
+		grpcretry.WithPerRetryTimeout(timeout),
+	}
+
+	cc, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Client{
+		api: ssov1.NewAuthClient(cc),
+	}, nil
+}
